e65dc75ea014463fd0224367efced581552e1fc7
origin <okwow123@24dcaea7d349>
2017-12-17 06:12:47 +0000

    add howtouse page

--- pinry/users/urls.py
+++ pinry/users/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns('',
     url(r'^register/$', CreateUser.as_view(), name='register'),
     url(r'^login/$', 'django.contrib.auth.views.login',
         {'template_name': 'users/login.html'}, name='login'),
+    url(r'^howtouse/$', 'django.contrib.auth.views.login',
+        {'template_name': 'users/howtouse.html'}, name='howtouse'),
     url(r'^logout/$', 'pinry.users.views.logout_user', name='logout'),
 )
